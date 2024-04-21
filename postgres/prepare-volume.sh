8a4029595ac2ffbee35aad0a0ab667779f30f6c6
AntonChubarov <achlp600@gmail.com>
2024-04-21 11:29:29 -0400

    fix: changed permissions for logs volume

--- postgres/prepare-volume.sh
+++ postgres/prepare-volume.sh
@@ -1,8 +1,7 @@
 mkdir -p volume/data
 chown -R :docker ./volume/data
-chmod -R 770 ./volume/data
+chmod -R 776 ./volume/data
 
 mkdir -p volume/logs
 chown -R :docker ./volume/logs
-chmod -R 770 ./volume/logs
-
+chmod -R 666 ./volume/logs

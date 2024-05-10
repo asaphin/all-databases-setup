3e4abba96df494265cff25b837be1eeefa809966
asaphin <anc@engineer.com>
2024-05-09 21:09:30 -0400

    feat: mysql custom log volume and config

--- mysql/prepare-volume.sh
+++ mysql/prepare-volume.sh
@@ -1,3 +1,11 @@
 mkdir -p volume/data
 chown -R :docker ./volume/data
 chmod -R 776 ./volume/data
+
+mkdir -p volume/config
+chown -R :docker ./volume/config
+chmod -R 776 ./volume/config
+
+mkdir -p volume/log
+chown -R :docker ./volume/log
+chmod -R 776 ./volume/log
